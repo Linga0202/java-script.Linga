549d3d295c84ad6eaf60ce20bb70270c8348887f
Linga0202 <153997664+Linga0202@users.noreply.github.com>
2024-01-27 09:32:50 +0530

    forms-js

---
+++ practice4/forms.js
@@ -0,0 +1,15 @@
+const names=document.getElementById('na')
+const Fname=document.getElementById('Fna')
+const email=document.getElementById('email')
+const password=document.getElementById('pass')
+const con=document.getElementById('c-pass')
+const conin=document.getElementById('confrom')
+conin.addEventListener('submit',submition)
+{
+    function submition(e)
+    {
+        console.log('hii')
+    }
+}
+
+ alert("hooo")
